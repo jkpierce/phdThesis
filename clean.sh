a143352b9ec2d15e809ea8c9254e00a4bdafa220
kevin <kevinkayaks@gmail.com>
2021-05-14 14:50:28 -0700

    directory structure and skeleton set up. Compilation successful

---
+++ clean.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+# this cleans out all of the auxilary and log files after compilation
+rm -f ch5expt/ch5expt.aux
+rm -f appendix.aux
+rm -f diss.toc
+rm -f laySum/laySum.aux
+rm -f ch3downDiff/ch3downDiff.aux
+rm -f ch2bedEle/ch2bedEle.aux
+rm -f diss.brf
+rm -f ack/ack.aux
+rm -f diss.log
+rm -f diss.lof
+rm -f ch6conc/ch6conc.aux
+rm -f diss.aux
+rm -f pref/pref.aux
+rm -f abs/abs.aux
+rm -f diss.lot
+rm -f ch4simul/ch4simul.aux
+rm -f diss.blg
+rm -f diss.bbl
+rm -f diss.synctex.gz
+rm -f /ch1intro/ch1intro.aux
+rm -f diss.out
